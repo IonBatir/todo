fcbb4c554770b682bd29ec1485e57a45d383df06
IonBatir <ionbatir@gmail.com>
2018-06-08 15:42:20 +0300

    Removed history

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,9 +4,5 @@ import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import rootReducer from './reducers'
 import thunk from 'redux-thunk'
 
-import createHistory from 'history/createBrowserHistory';
-
-export const history = createHistory();
-
 export const store = createStore(
   rootReducer, composeWithDevTools(applyMiddleware(thunk)));
