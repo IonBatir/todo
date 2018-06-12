5529f84adf458020f16ce2a88a805d35b97623f7
IonBatir <ionbatir@gmail.com>
2018-06-12 15:26:12 +0300

    Added server.js

--- src/App.js
+++ src/App.js
@@ -4,33 +4,12 @@ import Head from './containers/Head'
 import AddTodo from './containers/AddTodo'
 import './App.css'
 
-class App extends React.Component {
-  state = {
-    response: ''
-  }
-  componentDidMount() {
-    this.callApi()
-      .then(res => this.setState({ response: res.express }))
-      .catch(err => console.log(err));
-  }
-  callApi = async () => {
-    const response =  await fetch('/api/hello');
-    const body = await response.json();
-    if (response.status !== 200) throw Error(body.message);
-
-    return body;
-  }
-
-  render() {
-    return (
-      <div>
-        <Head />
-        <VisibleTodoList />
-        <AddTodo />
-        {this.state.response}    
-      </div>
-    )
-  }
-}
+const App = () => (
+  <div>
+    <Head />
+    <VisibleTodoList />
+    <AddTodo />  
+  </div>
+)
 
 export default App
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux'
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { store} from './redux/store'
+import { store } from './redux/store'
 
 render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
