d196f0e01c0328e63e5b23cafbb1a28d9fd26546
IonBatir <ionbatir@gmail.com>
2018-06-12 15:21:41 +0300

    Added server.js

--- server/server.js
+++ server/server.js
@@ -1,10 +1,40 @@
-const express = require('express');
+// server/app.js
 
-const app = express();
-const port = process.env.PORT || 5000;
+/** require dependencies */
+const express = require("express")
+const routes = require('./routes/')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const helmet = require('helmet')
 
-app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
+const app = express()
+const router = express.Router()
+const url = process.env.MONGODB_URI || "mongodb://localhost:27017/todos"
+
+/** connect to MongoDB datastore */
+try {
+    mongoose.connect(url, {
+        //useMongoClient: true
+    })
+    console.log('Connection established to', url); 
+} catch (error) {
+    console.log('Unable to connect to the mongoDB server. Error:', error);
+}
+
+let port = 5000 || process.env.PORT
+
+/** set up routes {API Endpoints} */
+routes(router)
+
+/** set up middlewares */
+app.use(cors())
+app.use(bodyParser.json())
+app.use(helmet())
+
+app.use('/api', router)
+
+/** start server */
+app.listen(port, () => {
+    console.log(`Server started at port: ${port}`);
 });
-
-app.listen(port, () => console.log(`Listening on port ${port}`));
