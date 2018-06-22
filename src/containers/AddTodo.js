e1aa1fdc164da8a48bc3fcd974b9085e59ae50f5
IonBatir <ionbatir@gmail.com>
2018-06-22 16:08:43 +0300

    Some changes to fit tests

--- src/containers/AddTodo.js
+++ src/containers/AddTodo.js
@@ -5,30 +5,42 @@ import AddIcon from '@material-ui/icons/Add';
 import Input from '@material-ui/core/Input';
 import { addTodo } from '../redux/actions'
 
-const AddTodo = ({ dispatch }) => {
-  let input
-  return (
-    <form
-      onSubmit={e => {
-        e.preventDefault()
-        if (!input.value.trim())
-          return
-        dispatch(addTodo(input.value))
-        input.value = ''
-      }}
-      >
+const mapDispatchToProps = dispatch => ({
+  addNewTodo: text => dispatch(addTodo(text))
+})
+
+export class AddTodo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''}
+  }
+
+  render() {  
+    const { addNewTodo } = this.props;
+    const onSubmit = e => {
+      e.preventDefault()
+      if (!this.state.value.trim())
+        return
+      addNewTodo(this.state.value)
+      this.setState({value: ''})
+    }
+    const onChange = e => this.setState({value: e.target.value})
+    return (
+      <form onSubmit={onSubmit}>
         <Input
         placeholder="New Task"
-        inputRef={node => input = node}
+        value={this.state.value}
+        onChange={onChange}
         inputProps={{
           'aria-label': 'New Task',
         }}
-        />
+        />        
         <Button id="fab" type="submit" variant="fab" color="primary">
           <AddIcon />
         </Button>
-    </form>
-  )
-} 
+      </form>
+    )    
+  }
+}
 
-export default connect()(AddTodo)
+export default connect(null, mapDispatchToProps)(AddTodo)
